380ad66f5df3a0979f93a815eef916e61fa35d68
Sebastián Mateo <sebabari@gmail.com>
2019-03-09 18:20:44 -0300

    Agregue busqueda de videos en youtube y archivo gitignore

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+DEVELOPER_KEY = "AIzaSyBu8Dts_inmtKjr_EMMTgGQahgeMO5H3S0"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def youtube_search(q, max_results):
+	youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+	# Call the search.list method to retrieve results matching the specified
+	# query term.
+	search_response = youtube.search().list(
+	q=q,
+	part="id,snippet",
+	maxResults=max_results
+	).execute()
+
+	videos = []
+
+	# Add each result to the appropriate list, and then display the lists of
+	# matching videos, channels, and playlists.
+	for search_result in search_response.get("items", []):
+		if search_result["id"]["kind"] == "youtube#video":
+			#videos.append("%s (%s)" % (search_result["snippet"]["title"],
+			#							search_result["id"]["videoId"]))
+			videos.append(search_result)
+	return videos
+
 
 # Create your views here.
 
@@ -7,3 +37,12 @@ def home(request):
 
 def about(request):
 	return render(request, 'blog/about.html')
+
+def search(request):
+	requestVideos = request.GET.get('video_name', '')
+	if requestVideos == '' :
+		listOfVideos = []
+	else:
+		listOfVideos = youtube_search(requestVideos, 10)
+	return render(request, 'blog/search.html', {'videos': listOfVideos})
+
