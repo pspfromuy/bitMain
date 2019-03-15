043253e6eb19e0accd43d2e753e5501bd740ea9f
pspfromuy <pablos91@gmail.com>
2019-03-15 04:59:01 -0300

    Perfil de usuario, Login, Logout, Registración en BD, Control de acceso a perfil y no se que más

--- users/views.py
+++ users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
-# Create your views here.
-def register(request):
-	form = UserCreationForm()
+
+def register(request): #Formulario para la registración de usuarios
+	if request.method == 'POST': 
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Cuenta creada exitosamente para {username}!')
+			return redirect('users-login')
+	else: 
+		form = UserRegisterForm() 
 	return render(request, 'users/register.html', {'form': form})
 
-def login(request):
-	return render(request, 'users/login.html')
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
